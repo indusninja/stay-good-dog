9d3e673928ca876501f215d44bb533d9dd112e0d
Dylan Thomson <skelinger@gmail.com>
2019-01-26 09:48:21 +0000

    Initial game manager setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] CommonItems;
+    public GameObject[] PersonalItems;
+    public GameObject[] SentimentalItems;
+
+    void Start()
+    {
+        
+    }
+}
