1ee38338a80f504d966b814acda52c41743b56d4
Dylan Thomson <skelinger@gmail.com>
2019-01-27 11:30:30 +0000

    No Comments

---
+++ Assets/DogKillZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogKillZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("OnTriggerEnter KILL ZONE");
+        if (collider.transform.CompareTag("Player"))
+        {
+            collider.gameObject.GetComponent<DogController> ().Kill();
+        }
+    }
+}
