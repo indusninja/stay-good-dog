7e96c18137fa3b76af18406864890070118af8c7
Dylan Thomson <skelinger@gmail.com>
2019-01-27 12:06:00 +0000

    No Comments

--- Assets/DogKillZone.cs
+++ Assets/DogKillZone.cs
@@ -6,7 +6,6 @@ public class DogKillZone : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collider)
     {
-        Debug.Log("OnTriggerEnter KILL ZONE");
         if (collider.transform.CompareTag("Player"))
         {
             collider.gameObject.GetComponent<DogController> ().Kill();
