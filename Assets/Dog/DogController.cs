10a872e150cce4c6e5fa069e18fcb18249b4bb73
Oskar Korošec <oskar.korosec@gmail.com>
2019-01-27 09:20:27 +0000

    car kill dog very sad

--- Assets/Dog/DogController.cs
+++ Assets/Dog/DogController.cs
@@ -27,6 +27,7 @@ public class DogController : MonoBehaviour
     private float m_lookSensitivity = 3.0f;
     private bool m_cursorIsLocked = true;
     private Vector3 previousRotation = Vector3.zero;
+    private bool isDead = false;
 
     private SoundManager SoundManagerInstance;
 
@@ -38,6 +39,8 @@ public class DogController : MonoBehaviour
 
     [Header("The Camera the player looks through")]
     public Camera m_Camera;
+
+    public PhysicMaterial deadDogMaterial;
 
     // Use this for initialization
     private void Start()
@@ -52,65 +55,69 @@ public class DogController : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        m_MovX = Input.GetAxisRaw("Horizontal");
-        m_MovY = Input.GetAxisRaw("Vertical");
-        //Debug.Log(m_MovX);
-
-        //m_moveHorizontal = transform.right * m_MovX;
-        m_moveHorizontal = Vector3.zero;
-        m_movVertical = transform.forward * m_MovY;
-
-        m_velocity = (m_moveHorizontal + m_movVertical).normalized * speed;
-
-        //mouse movement 
-        m_yRot = Input.GetAxisRaw("Mouse X");
-        m_rotation = new Vector3(0, m_yRot, 0) * m_lookSensitivity;
-
-        m_xRot = Input.GetAxisRaw("Mouse Y");
-        m_cameraRotation = new Vector3(m_xRot, 0, 0) * m_lookSensitivity;
-
-        //apply camera rotation
-        
-        //move the actual player here
-        if (m_velocity != Vector3.zero)
+        if (!isDead)
         {
-            //m_Rigid.MovePosition(m_Rigid.position + m_velocity * Time.fixedDeltaTime);
-            m_Rigid.AddForce(m_velocity, ForceMode.Acceleration);
-        }
-        else if (m_Rigid.velocity.magnitude > 1f)
-        {
-            Vector3 stoppingForceVector = m_Rigid.velocity.normalized * stoppingForce;
-            stoppingForceVector.y = 0;
-            m_Rigid.AddForce(stoppingForceVector);
-        }
-        else
-        {
-            m_Rigid.velocity = new Vector3(0f, m_Rigid.velocity.y, 0f);
-        }
 
-        if (m_rotation != Vector3.zero)
-        {
-            //rotate the camera of the player
-            m_Rigid.MoveRotation(m_Rigid.rotation * Quaternion.Euler(m_rotation));
+            m_MovX = Input.GetAxisRaw("Horizontal");
+            m_MovY = Input.GetAxisRaw("Vertical");
+            //Debug.Log(m_MovX);
+
+            //m_moveHorizontal = transform.right * m_MovX;
+            m_moveHorizontal = Vector3.zero;
+            m_movVertical = transform.forward * m_MovY;
+
+            m_velocity = (m_moveHorizontal + m_movVertical).normalized * speed;
+
+            //mouse movement 
+            m_yRot = Input.GetAxisRaw("Mouse X");
+            m_rotation = new Vector3(0, m_yRot, 0) * m_lookSensitivity;
+
+            m_xRot = Input.GetAxisRaw("Mouse Y");
+            m_cameraRotation = new Vector3(m_xRot, 0, 0) * m_lookSensitivity;
+
+            //apply camera rotation
+
+            //move the actual player here
+            if (m_velocity != Vector3.zero)
+            {
+                //m_Rigid.MovePosition(m_Rigid.position + m_velocity * Time.fixedDeltaTime);
+                m_Rigid.AddForce(m_velocity, ForceMode.Acceleration);
+            }
+            else if (m_Rigid.velocity.magnitude > 1f)
+            {
+                Vector3 stoppingForceVector = m_Rigid.velocity.normalized * stoppingForce;
+                stoppingForceVector.y = 0;
+                m_Rigid.AddForce(stoppingForceVector);
+            }
+            else
+            {
+                m_Rigid.velocity = new Vector3(0f, m_Rigid.velocity.y, 0f);
+            }
+
+            if (m_rotation != Vector3.zero)
+            {
+                //rotate the camera of the player
+                m_Rigid.MoveRotation(m_Rigid.rotation * Quaternion.Euler(m_rotation));
+            }
+
+            if (m_Camera != null)
+            {
+                //negate this value so it rotates like a FPS not like a plane
+                m_Camera.transform.Rotate(-m_cameraRotation);
+            }
+
+            if (m_Rigid.velocity.magnitude > maxVelocity)
+            {
+                m_Rigid.velocity = m_Rigid.velocity.normalized * maxVelocity;
+            }
+
+            BobbingCamera(m_Camera.transform.localRotation, m_velocity, m_MovY, m_MovX);
+
+            SoundManagerInstance.SetCurrentWalkingSurface(
+                FindMovementSurface(previousRotation - m_Rigid.transform.rotation.eulerAngles, m_velocity));
+
+            previousRotation = m_Rigid.transform.rotation.eulerAngles;
         }
-
-        if (m_Camera != null)
-        {
-            //negate this value so it rotates like a FPS not like a plane
-            m_Camera.transform.Rotate(-m_cameraRotation);
-        }
-
-        if (m_Rigid.velocity.magnitude > maxVelocity)
-        {
-            m_Rigid.velocity = m_Rigid.velocity.normalized * maxVelocity;
-        }
-
-        BobbingCamera(m_Camera.transform.localRotation, m_velocity, m_MovY, m_MovX);
-
-        SoundManagerInstance.SetCurrentWalkingSurface(
-            FindMovementSurface(previousRotation - m_Rigid.transform.rotation.eulerAngles, m_velocity));
-
-        previousRotation = m_Rigid.transform.rotation.eulerAngles;
 
         InternalLockUpdate();
     }
@@ -223,5 +230,13 @@ public class DogController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+    }
+
+    public void Kill()
+    {
+        // woof woof oouughgjj
+        m_Rigid.constraints = RigidbodyConstraints.None;
+        gameObject.GetComponent<SphereCollider>().material = deadDogMaterial;
+        isDead = true;
     }
 }
--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,73 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour {
-	
-	public float accelleration = 10f;
-	public float maxSpeed = 10f;
-	public float turnForce = 10f;
-	public Transform[] pathPoints;
-	public PathsController controller;
+public class CarController : MonoBehaviour
+{
+
+    public float accelleration = 10f;
+    public float maxSpeed = 10f;
+    public float turnForce = 10f;
+    public Transform[] pathPoints;
+    public PathsController controller;
 
     //public RandomSoundPlayer carSounds;
 
-	public Material[] materials;
+    public Material[] materials;
 
-	public bool loop = false;
+    public bool loop = false;
 
-	private Rigidbody rb;
+    private Rigidbody rb;
 
-	private bool running = false;
+    private bool running = false;
 
-	public int nextPathPoint = 0;
+    public int nextPathPoint = 0;
 
-	public float collisionForce = 10f;
+    public float collisionForce = 10f;
 
-	public float groundCheckHeight = 2.0f;
+    public float groundCheckHeight = 2.0f;
 
-	public LayerMask layerMask;
+    public LayerMask layerMask;
 
-	void OnTriggerEnter(Collider collider) {
-		if (collider.tag == "carNavPoint") {
-			for (var i = 0; i < pathPoints.Length; i++) {
-				if(pathPoints[i] == collider.transform) {
-					nextPathPoint = i + 1;
-				}
-			}
-			if(collider.transform == pathPoints[pathPoints.Length-1]) {
-				if (loop) {
-					nextPathPoint = 0;
-				} else {
-					running = false;
-					Destroy (gameObject);
-				}
-			}
-		}
-	}
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "carNavPoint")
+        {
+            for (var i = 0; i < pathPoints.Length; i++)
+            {
+                if (pathPoints[i] == collider.transform)
+                {
+                    nextPathPoint = i + 1;
+                }
+            }
+            if (collider.transform == pathPoints[pathPoints.Length - 1])
+            {
+                if (loop)
+                {
+                    nextPathPoint = 0;
+                }
+                else
+                {
+                    running = false;
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
 
-	public void StopRunning() {
-		running = false;
-	}
 
-	public void SetPathController(PathsController pc) {
-		controller = pc;
+    public void StopRunning()
+    {
+        running = false;
+    }
+
+    public void SetPathController(PathsController pc)
+    {
+        controller = pc;
         /*
 		Material randomMat = materials [Random.Range (0, materials.Length)];
 		GetComponent<MeshRenderer> ().material = randomMat;
         */
 
-		pathPoints = controller.GetPathPoints();
-		rb = GetComponent<Rigidbody> ();
-		StartMovement ();
-	}
+        pathPoints = controller.GetPathPoints();
+        rb = GetComponent<Rigidbody>();
+        StartMovement();
+    }
 
-	// Use this for initialization
-	public void SetPathPoints (Transform[] path) {	
-		pathPoints = path;
-	}
+    // Use this for initialization
+    public void SetPathPoints(Transform[] path)
+    {
+        pathPoints = path;
+    }
 
-	public void StartMovement() {
-		running = true;
+    public void StartMovement()
+    {
+        running = true;
 
         /*
         if (carSounds != null && !carSounds.audioSource.isPlaying)
@@ -78,59 +92,74 @@ public class CarController : MonoBehaviour {
         */
     }
 
-	void FixedUpdate() {
-		if(running) {
-			CheckIfGrounded ();
-			// Rotate car towards next point with add torque
-			Vector3 targetDelta = pathPoints[nextPathPoint].position - transform.position;
-			float angleDiff = Vector3.Angle (transform.forward, targetDelta);
-			Vector3 cross = Vector3.Cross (transform.forward, targetDelta);
-			rb.AddTorque (cross * angleDiff * turnForce);
+    void FixedUpdate()
+    {
+        if (running)
+        {
+            CheckIfGrounded();
+            // Rotate car towards next point with add torque
+            Vector3 targetDelta = pathPoints[nextPathPoint].position - transform.position;
+            float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+            Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+            rb.AddTorque(cross * angleDiff * turnForce);
 
-			// Add forward force
-			rb.AddForce (transform.forward * accelleration);
-			if (rb.velocity.magnitude > maxSpeed) {
-				rb.velocity = Vector3.ClampMagnitude (rb.velocity, maxSpeed);
-			}
-		}
-	}
+            // Add forward force
+            rb.AddForce(transform.forward * accelleration);
+            if (rb.velocity.magnitude > maxSpeed)
+            {
+                rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+            }
+        }
+    }
 
-	void CheckIfGrounded() {
-		RaycastHit hit;
-		if (Physics.Raycast (transform.position, transform.up, out hit, layerMask)) {
-			if (hit.collider != null && hit.distance < groundCheckHeight) {
-				Disable ();
-			}
-		}
-	}
+    void CheckIfGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up, out hit, layerMask))
+        {
+            if (hit.collider != null && hit.distance < groundCheckHeight)
+            {
+                Disable();
+            }
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-		if (collision.transform.CompareTag ("Car")) {
+        if (collision.transform.CompareTag("Car"))
+        {
             /*
 			Disable ();
 			collision.gameObject.GetComponent<CarController> ().Disable ();
             */
 
-			GetComponent<Rigidbody> ().AddExplosionForce (collisionForce, transform.position, 100f);
-			collision.transform.GetComponent<Rigidbody> ().AddExplosionForce (collisionForce, transform.position, 100f);
-			collision.transform.GetComponent<Rigidbody> ().AddExplosionForce (50, collision.contacts [0].point, 10f);
-		}
+            GetComponent<Rigidbody>().AddExplosionForce(collisionForce, transform.position, 100f);
+            collision.transform.GetComponent<Rigidbody>().AddExplosionForce(collisionForce, transform.position, 100f);
+            collision.transform.GetComponent<Rigidbody>().AddExplosionForce(50, collision.contacts[0].point, 10f);
+        }
+        else if (collision.transform.CompareTag("Player"))
+        {
+            DogController dog = collision.gameObject.GetComponent<DogController>();
+            dog.Kill();
+        }
     }
 
-	public void Disable() {
-		StopRunning ();
-		GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.None;
-		DestroyCar ();
-	}
+    public void Disable()
+    {
+        StopRunning();
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        DestroyCar();
+    }
 
-	public void DestroyCar() {
-		StartCoroutine (DestroyAfter());
-	}
+    public void DestroyCar()
+    {
+        StartCoroutine(DestroyAfter());
+    }
 
-	public IEnumerator DestroyAfter(float seconds = 5) {
-		yield return new WaitForSeconds (seconds);
-		running = false;
-		Destroy (gameObject);
-	}
+    public IEnumerator DestroyAfter(float seconds = 5)
+    {
+        yield return new WaitForSeconds(seconds);
+        running = false;
+        Destroy(gameObject);
+    }
 }
